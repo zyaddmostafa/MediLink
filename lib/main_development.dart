38586debb56d6f41faa9bad5ff813a4d396d317e
Zyad Mostafa <zyadmo026@gmail.com>
2025-07-16 10:36:36 +0300

    feat: integrate Firebase and Fastlane for app distribution and configuration

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,11 @@
 import 'package:doctor_appoinment/feature/home_screen.dart';
+import 'package:doctor_appoinment/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
