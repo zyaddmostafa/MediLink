d035c1c62ab42d41ceed5b88b403a3d6edc15c3d
Zyad Mostafa <zyadmo026@gmail.com>
2025-07-16 03:27:24 +0300

    core: update dependencies and add app text styles
    - Updated dependencies in pubspec.yaml, including bloc, dio, flutter_bloc, and others.
    - Added new dev_dependencies for build_runner and json_serializable.
    - Created app_text_styles.dart to define text styles using Google Fonts (Open Sans) with various weights and sizes.

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xFF2E6FF3);
+  static const Color black = Color(0xFF33384B);
+  static const Color white = Color(0xFFF9FAFB);
+  static const Color red = Color(0xFFED5E5E);
+  static const Color babyBlue = Color(0xFFE8F0FF);
+  static const Color grey = Color(0xFF7D8A95);
+}
